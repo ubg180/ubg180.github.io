5e9289a4a049356281cb62f96c9fba1e1d4c91cd
maxlud09 <147512425+maxlud09@users.noreply.github.com>
2023-12-06 19:27:26 +0100

    Update script.js

--- assets/firebase/script/script.js
+++ assets/firebase/script/script.js
@@ -23,7 +23,7 @@ function login() {
 				logUserActivity(userAction);
                 loader.style.display = "none";
                 home.style.display = "block";
-		    		showalert("Ersatzdomains","Checke jetzt die Ersatzdomains. Klicke einfach auf Ersatzdomains in der Homebar.", 5000)
+		    		showalert("Neues Video","Checke jetzt das Neue Video. Klicke einfach auf Allein Allein in der Homebar. Wir wünschen viel Spaß beim Anschauen.", 5000)
 
             })
             .catch(function (error) {
