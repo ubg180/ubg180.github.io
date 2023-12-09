2fbb6d2fa315435b2982354e9d38d42a221c1505
maxlud09 <147512425+maxlud09@users.noreply.github.com>
2023-12-09 11:20:13 +0100

    Create störungen_updates.js

---
+++ assets/firebase/script/störungen_updates.js
@@ -0,0 +1,50 @@
+// Auf die Datenbank zugreifen
+    var database4 = firebase.database();
+
+    // Auf das Dokument "disruptions" hören
+    var ref = database4.ref("disruptions");
+
+    // Eine Variable für den aktuellen Index der Störung
+    var index = 0;
+
+    // Eine Funktion, die die Leiste mit der nächsten Störung aktualisiert
+    function updateBar() {
+      // Die Leiste auswählen
+      var bar = document.getElementById("bar");
+
+      // Die Störungen aus der Datenbank lesen
+      ref.once("value", function(snapshot) {
+        // Die Störungen in ein Array umwandeln
+        var disruptions = snapshot.val();
+
+        // Die Anzahl der Störungen ermitteln
+        var length = disruptions.length;
+
+        // Die aktuelle Störung aus dem Array holen
+        var disruption = disruptions[index];
+
+        // Die Leiste mit der Störung füllen
+        bar.innerHTML = disruption;
+
+        // Die Leiste animieren
+        bar.classList.add("animate");
+
+        // Den Index um eins erhöhen
+        index++;
+
+        // Wenn der Index das Ende des Arrays erreicht hat, wieder von vorne anfangen
+        if (index == length) {
+          index = 0;
+        }
+
+          bar.classList.remove("animate");
+
+      });
+    }
+
+    // Die Funktion zum ersten Mal aufrufen
+    updateBar();
+
+    // Die Funktion alle 2 Sekunden wiederholen
+    setInterval(updateBar, 5000);
+
