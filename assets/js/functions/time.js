b7df5a3efc72bd8c2fe984b226cfabc1f4671a80
ubg180 <158818777+ubg180@users.noreply.github.com>
2024-02-16 09:37:35 +0100

    Update time.js

--- assets/js/functions/time.js
+++ assets/js/functions/time.js
@@ -16,7 +16,7 @@ function getCurrentDate() {
   const month = now.getMonth() + 1;
   const year = now.getFullYear();
   const weekday = weekdays[now.getDay()];
-  return `${weekday}, 1234 ${day}.${month}.${year}`;
+  return `${weekday}, der ${day}.${month}.${year}`;
 }
 
 // Aktualisiere Uhrzeit und Datum
